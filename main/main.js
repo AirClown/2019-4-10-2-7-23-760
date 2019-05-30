a414ec4a2cea1439902bc49f92524078f6bdb11c
AirClown <yu.shichao@foxmail.com>
2019-05-30 11:59:05 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var fee = 6.0;
+
+    if(inputs.distance>2){
+    	fee += (inputs.distance - 2) * 0.8;
+    	if(inputs.distance > 8) fee += (inputs.distance - 8) * 0.4;
+    }
+
+    fee += 0.25 * inputs.parkTime;
+
+    if(fee - parseInt(fee) >= 0.5) fee = parseInt(fee) + 1;
+    else fee = parseInt(fee);
+    
+    return fee;
 };
